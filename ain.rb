6ef65f54a1a1bb535b0a728f20816b99da28e253
Jonas Lundberg <jonasl@iamjonas.me>
2021-09-08 10:18:15 +0200

    Release ain v1.1.0

--- ain.rb
+++ ain.rb
@@ -5,29 +5,29 @@
 class Ain < Formula
   desc "Ain is a terminal API client. It's an alternative to postman, paw or insomnia."
   homepage "https://github.com/jonaslu/ain"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/jonaslu/ain/releases/download/v1.0.0/ain_1.0.0_mac_os_x86_64.tar.gz"
-      sha256 "94471d7e0fe5dab167a13f59165120d9513afaaf489980b47d9e07f27de0c5ed"
+      url "https://github.com/jonaslu/ain/releases/download/v1.1.0/ain_1.1.0_mac_os_x86_64.tar.gz"
+      sha256 "d1b8c8707a2cd25cc4c9d4fb9a21370b523f894c33aabdeafa545e3d0ae49c19"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/jonaslu/ain/releases/download/v1.0.0/ain_1.0.0_mac_os_arm64.tar.gz"
-      sha256 "15ec69475c108c0d8ee808f63533f00c6564eda3d58ec16acc8411311008c706"
+      url "https://github.com/jonaslu/ain/releases/download/v1.1.0/ain_1.1.0_mac_os_arm64.tar.gz"
+      sha256 "42f85d3965cd2eee0cd4ee31ac464f1c57ee56dfbb34122c402ad7f3aadaf5b2"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/jonaslu/ain/releases/download/v1.0.0/ain_1.0.0_linux_x86_64.tar.gz"
-      sha256 "dd65f83002d51d7c8b7e892041392ae4c13eb5470d35bbbce2bc9b7806bdafd2"
+      url "https://github.com/jonaslu/ain/releases/download/v1.1.0/ain_1.1.0_linux_x86_64.tar.gz"
+      sha256 "9889024badd192e734dc3542d28341deb41bc0c8f23e405b27a2eff0bb5af3cb"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jonaslu/ain/releases/download/v1.0.0/ain_1.0.0_linux_arm64.tar.gz"
-      sha256 "561a2aa36d55f835e786cd46994f8be6039efa5e884ce4a52dc7a55bcd3c2f57"
+      url "https://github.com/jonaslu/ain/releases/download/v1.1.0/ain_1.1.0_linux_arm64.tar.gz"
+      sha256 "ecf41d3a62bd19372768af5d702ad9f9249514c749829c655a67add46a2e5e86"
     end
   end
 
